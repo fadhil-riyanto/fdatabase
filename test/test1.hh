e140efec9ea9b5f913abc3f569e9407e397f5d09
fadhil_riyanto <id.fadhil.riyanto@gmail.com>
2021-11-16 14:41:09 +0700

    test1 : ok
    db.fdb : NULL
    result : overwrite

--- test/test1.hh
+++ test/test1.hh
@@ -4,9 +4,9 @@ function main(): noreturn {
     require(__DIR__ . "/../src/Utils.hh");
     require(__DIR__ . '/../src/main.hh');
     $db = new fdatabase("db.fdb", dict[
-        "set_load_file" => false
+        "set_load_file" => true
     ]);
-    $db->set("foUoe1", "apaan?");
-    echo $db->get("foUoe1");
+    $db->set("foUoe", "bare", true);
+    $db->commit();
     exit(0);
 }
